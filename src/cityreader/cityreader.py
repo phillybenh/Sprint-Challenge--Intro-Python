4b2eaf11e156a32c6995065a203e09949463db89
Ben Haus <ben@little-haus.com>
2020-06-05 13:30:56 -0400

    passing test_cityreader

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,11 +1,24 @@
+# import csv module
+import csv
+
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+
+
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+    # def __str__(self):
+    #     return f"{self.name}, {self.lat}, {self.lon}"
 
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
-# In the body of the `cityreader` function, use Python's built-in "csv" module 
+# In the body of the `cityreader` function, use Python's built-in "csv" module
 # to read this file so that each record is imported into a City instance. Then
 # return the list with all the City instances from the function.
 # Google "python 3 csv" for references and use your Google-fu for other examples.
@@ -16,11 +29,18 @@
 # should not be loaded into a City object.
 cities = []
 
+
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
+  # For each city record, create a new City instance and add it to the
   # `cities` list
-    
+    with open("cities.csv") as cities_file: 
+        reader = csv.reader(cities_file, delimiter=',')
+        line = 0
+        for row in reader:
+            if line != 0:
+                cities.append(City(str(row[0]), float(row[3]), float(row[4])))
+                line += 1
     return cities
 
 cityreader(cities)
